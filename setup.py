6c2f6e91eade0cf96da3bedc87794206a8090ddf
jack-michaud <mrmichaud42@gmail.com>
2017-05-17 23:38:29 -0400

    Package files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'iPick',
+  packages = ['iPick'],
+  version = '0.1',
+  description = 'Scrapes HTML by structure.',
+  author = 'Jack Michaud',
+  author_email = 'jack.a.michaud@gmail.com',
+  url = 'https://github.com/jack-michaud/IcePick',
+  download_url = 'https://github.com/jack-michaud/IcePick/archive/0.1.tar.gz',
+  keywords = ['icepick', 'facebook', 'scraping', 'useful'], # Ha, biased!
+  classifiers = [],
+)
